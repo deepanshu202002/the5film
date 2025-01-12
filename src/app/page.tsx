07e40c5e0cea682227a18b2380f2b569b1a6fff7
deepanshu202002 <deepanshu.moorjani20@gmail.com>
2025-01-13 01:21:46 +0530

    bug fixing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Navbar from "../components/Navbar";
 import Carousel from "../components/Carousel";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 export default function Home() {
   return (
     <div>

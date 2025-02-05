1c37ffb6881302402ec2155f0278b85b4fee82e4
deepanshu202002 <deepanshu.moorjani20@gmail.com>
2025-02-05 18:27:57 +0530

    services component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import Navbar from "../components/Navbar";
 import Carousel from "../components/Carousel";
 import FiveImages from "../components/FiveImages";
+import Services from "../components/Services";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col">
       <Navbar />
-      <Carousel /> {/* Use the Carousel component here */}
+      <Carousel />
       <FiveImages />
+      <div className="mt-16 overflow-visible">
+        <Services />
+      </div>
     </div>
   );
 }
